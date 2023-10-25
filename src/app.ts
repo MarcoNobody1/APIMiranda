b19004fb8aa99173aa2145dd7d54986bc348f744
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-25 18:49:47 +0200

    Se formatea app

--- src/app.ts
+++ src/app.ts
@@ -3,21 +3,19 @@ import cors from 'cors'
 import { bookingsController } from "./controllers/bookings";
 import { roomsController } from "./controllers/rooms";
 import { loginController } from "./controllers/login";
+import authMiddleware from "./middleware/auth";
 
 export const app = express()
 
-// middlewares
 app.use(cors())
 app.use(express.json())
 
-
-
-// public routes
-// app.use('/login', loginController)
+// public routes & middleware
+app.use('/login', loginController)
+app.use(authMiddleware)
 
 // private routes
 app.use('/bookings', bookingsController)    
 app.use('/rooms', roomsController)   
-app.use('/login', loginController)
 // app.use('/users', usersController)  
 // app.use('/contacts', contactsController)    
