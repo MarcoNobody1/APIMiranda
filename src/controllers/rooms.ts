de3d1b495a5f6e394235431e6fc1a5c65e75007c
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-25 15:38:48 +0200

    Se implementa service de rooms al controlador de rooms

---
+++ src/controllers/rooms.ts
@@ -0,0 +1,55 @@
+import { Request, Response, Router } from "express";
+import { RoomInterface } from "../models/Rooms";
+import { RoomService } from "../services/rooms";
+
+export const roomsController = Router();   
+
+roomsController.get("/", async (_req: Request, res: Response) => {
+  try {
+    const result = await RoomService.getAllrooms();
+    res.send(result);
+  } catch (error) {
+    res.status(500).send("Error al obtener las reservas.");
+  }
+});
+
+roomsController.get("/:id", async (req: Request, res: Response) => {
+    try {
+      const result = await RoomService.getOneRoom(parseInt(req.params.id));
+        res.send(result);
+
+    } catch (error) {
+      res.status(400).send(`${error}`);
+    }
+  }
+);
+
+roomsController.delete("/:id", async (req: Request, res: Response) => {
+    try {
+      const result = await RoomService.delete(parseInt(req.params.id));
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(400).send(`${error}`);
+    }
+  }
+);
+
+roomsController.put("/:id", async (req: Request,res: Response) => {
+    try {
+      const result = await RoomService.updateRoom(parseInt(req.params.id), req.body);
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(400).send(`${error}`);
+    }
+  }
+);
+
+roomsController.post("/", async (req: Request<RoomInterface>, res: Response) => {
+    try {
+      const result = await RoomService.postNewRoom(req.body);
+      res.status(200).send(`Your Room is number ${result}`);
+    } catch (error) {
+      res.status(500).send(`${error}`);
+    }
+  }
+);
