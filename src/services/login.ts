0c41a170e65161acf5aa67aee9458ecac800ae36
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-25 18:49:27 +0200

    Se termina logica de la funcion verifyJWT

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,33 +1,37 @@
-import jwt from 'jsonwebtoken';
-import 'dotenv/config';
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 const defaultUser = {
-    user: "admin",
-    password: "admin",
-  };
+  user: "admin",
+  password: "admin",
+};
 
-  const secretToken : string = process.env.SECRET_TOKEN || '';
-  
-  async function login(user: string, password: string) {
-   if ( defaultUser.user !== user || defaultUser.password !== password ){
-    throw new Error('Username or Password Incorrect!');
-   }
-   return signJWT({user})
+const secretToken: string = process.env.SECRET_TOKEN || "";
+
+async function login(user: string, password: string) {
+  if (defaultUser.user !== user || defaultUser.password !== password) {
+    throw new Error("Username or Password Incorrect!");
   }
-  
-  function signJWT(payload: { user: string }) {
-    const token = jwt.sign(payload, secretToken, { expiresIn: '1h' });
-    return {payload, token}
-  }
-  
-  function verifyJWT(token: string) {
-    // Verify the jwt token
-  }
-  
-  const authService = {
-    login,
-    signJWT,
-    verifyJWT,
-  };
-  
-  export default authService;
+  return signJWT({ user });
+}
+
+function signJWT(payload: { user: string }) {
+  const token = jwt.sign(payload, secretToken, { expiresIn: "1h" });
+  return { payload, token };
+}
+
+function verifyJWT(token: string) {
+  jwt.verify(token, secretToken, (err, token) => {
+    if (err) throw new Error("You are not authorized.");
+    return token;
+  });
+
+}
+
+const authService = {
+  login,
+  signJWT,
+  verifyJWT,
+};
+
+export default authService;
