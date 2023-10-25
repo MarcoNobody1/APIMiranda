96cdeb6bdc5d4081e872d9f5f567d433b8b79e24
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-25 15:37:06 +0200

    Se termina logica del service de bookings

--- src/services/bookings.ts
+++ src/services/bookings.ts
@@ -1,33 +1,65 @@
-import { bookingsController } from "../controllers/bookings";
 import bookingsData from "../data/Bookings.json";
 import { BookingInterface } from "../models/Bookings";
 
 export const bookings = bookingsData as BookingInterface[];
 
-async function get() {
-  // Get all bookings from json file
+async function getAllBookings() {
+  //logica futura en DB.
+  const data = await bookings;
+  if (data.length === 0) throw new Error("No existen reservas.");
+  return data;
 }
 
-async function getById(bookingId: number) {
-  // Get a booking by id from json file
+async function getOneBooking(bookingId: number) {
+  //logica futura en DB.
+  const data = await bookings.filter(
+    (booking) => booking.guest.id_reserva === bookingId.toString()
+  );
+  if (data.length === 0) throw new Error("No hay ninguna reserva con ese id.");
+  return data;
 }
 
-async function post(booking: BookingInterface) {
-  // Save a booking to json file
+async function postNewBooking(booking: BookingInterface) {
+  //logica futura en DB.
+  const initialLength = bookings.length;
+  const data = await bookings.push(booking);
+  if(data === initialLength) throw new Error("Tu reserva no se añadio correctamente.")
+  return data;
 }
 
-async function put(bookingId: number, update: Partial<BookingInterface>) {
-  // Update a booking by id and save to json file
+async function updateBooking(
+  bookingId: number,
+  update: Partial<BookingInterface>
+) {
+  //logica futura en DB.
+  const bookingIndex = await bookings.findIndex(
+    (booking) => booking.guest.id_reserva === bookingId.toString()
+  );
+
+  if (bookingIndex === -1) throw new Error("No puedes modificar una reserva que no existe.")
+  const data = [...bookings];
+  Object.assign(data[bookingIndex], update);
+  return data;
+
 }
 
-async function _delete(bookingId: number) {
-  // Delete a booking by id from json file
+async function deleteBooking(bookingId: number) {
+  //logica futura en DB.
+
+  const bookingIndex = await bookings.findIndex(
+    (booking) => booking.guest.id_reserva === bookingId.toString()
+  );
+
+  if (bookingIndex === -1) throw new Error("No hay ninguna reserva con ese id.");
+
+  const data = bookings.splice(bookingIndex, 1);
+  return data;
 }
 
 export const bookingService = {
-  get,
-  getById,
-  post,
-  put,
-  delete: _delete,
+  getAllBookings,
+  getOneBooking,
+  postNewBooking,
+  updateBooking,
+  delete: deleteBooking,
 };
