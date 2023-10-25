b9dd1156d7faa4c2c132cafce655fe6ec6c5a307
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-25 15:37:46 +0200

    Se implementa service de bookings al controlador de bookings

--- src/controllers/bookings.ts
+++ src/controllers/bookings.ts
@@ -1,57 +1,55 @@
 import { Request, Response, Router } from "express";
 import { BookingInterface } from "../models/Bookings";
-import bookingsData from "../data/Bookings.json";
+import { bookingService } from "../services/bookings";
 
-export const bookingsController = Router();
+export const bookingsController = Router();   
 
-bookingsController.get("/", (req: Request, res: Response) => {
-  res.send(bookingsData);
-});
-
-bookingsController.get(
-  "/:id",
-  (req: Request<{ id: number }>, res: Response) => {
-    const id = req.params.id.toString();
-    const result = bookingsData.filter(
-      (booking) => booking.guest.id_reserva === id
-    );
+bookingsController.get("/", async (_req: Request, res: Response) => {
+  try {
+    const result = await bookingService.getAllBookings();
     res.send(result);
+  } catch (error) {
+    res.status(500).send("Error al obtener las reservas.");
   }
-);
-
-bookingsController.delete(
-  "/:id",
-  (req: Request<{ id: number }>, res: Response) => {
-    const id = req.params.id.toString();
-    const indexBooking = bookingsData.findIndex(
-      (booking) => booking.guest.id_reserva === id
-    );
-    bookingsData.splice(indexBooking, 1);
-    res.status(200).send(`Booking ${id} deleted`);
-  }
-);
-
-bookingsController.put(
-  "/:id",
-  (req: Request<{ id: number; data: BookingInterface }>, res: Response) => {
-    const id = req.params.id.toString();
-    const selectedBooking = req.body.data;
-
-    const bookingIndex = bookingsData.findIndex(
-      (booking) => booking.guest.id_reserva === id
-    );
-
-    const modifiedBooking = (bookingsData[bookingIndex] = {
-      ...bookingsData[bookingIndex],
-      ...selectedBooking,
-    });
-
-    res.send(modifiedBooking);
-  }
-);
-
-bookingsController.post("/", (req: Request<BookingInterface>, res: Response) => {
-  const newBooking = req.body;
-  bookingsData.push(newBooking);
-  res.status(200).send(`Booking ${newBooking.guest.id_reserva} added`);
 });
+
+bookingsController.get("/:id", async (req: Request, res: Response) => {
+    try {
+      const result = await bookingService.getOneBooking(parseInt(req.params.id));
+        res.send(result);
+
+    } catch (error) {
+      res.status(400).send(`${error}`);
+    }
+  }
+);
+
+bookingsController.delete("/:id", async (req: Request, res: Response) => {
+    try {
+      const result = await bookingService.delete(parseInt(req.params.id));
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(400).send(`${error}`);
+    }
+  }
+);
+
+bookingsController.put("/:id", async (req: Request,res: Response) => {
+    try {
+      const result = await bookingService.updateBooking(parseInt(req.params.id), req.body);
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(400).send(`${error}`);
+    }
+  }
+);
+
+bookingsController.post("/", async (req: Request<BookingInterface>, res: Response) => {
+    try {
+      const result = await bookingService.postNewBooking(req.body);
+      res.status(200).send(`Your booking is number ${result}`);
+    } catch (error) {
+      res.status(500).send(`${error}`);
+    }
+  }
+);
