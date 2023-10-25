176574c6ea239b8018e5456e917b6a743225dbe8
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-25 17:21:23 +0200

    Se crea archivo auth

---
+++ src/middleware/auth.ts
@@ -0,0 +1,10 @@
+import authService from '../services/login'
+import { NextFunction, Request, Response } from 'express'
+
+export default function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // Get the jwt token from the headers and verify it
+}
