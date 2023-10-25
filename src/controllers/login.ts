0830ec73d10c9b7786b33ada6a901f2dccea66df
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-25 17:20:44 +0200

    Se crea controlador de login y se implementa el servicio de login

---
+++ src/controllers/login.ts
@@ -0,0 +1,16 @@
+import {  Request, Response, Router } from "express";
+import authService from "../services/login";
+
+export const loginController = Router(); 
+
+loginController.post("/", async(req: Request<{user: string, password: string}>, res: Response) => {
+try {
+    const username = req.body.user;
+    const password = req.body.password;
+    const result = await authService.login(username, password);
+    res.send(result)
+} catch (error) {
+    res.send(`${error}`)
+}
+
+})
