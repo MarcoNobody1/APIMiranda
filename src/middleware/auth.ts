1be9a67b0fd8e9862f313be063e0629cde01e6cb
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-26 11:40:32 +0200

    Se cambia tipado de la interfaz de RequestHeaders

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,7 +2,7 @@ import { RequestHeadersInterface } from '../models/RequestHeaders';
 import authService from '../services/login'
 import { NextFunction, Request, Response } from 'express'
 
-export default function authMiddleware(req: Request & {headers: RequestHeadersInterface}, res: Response, next: NextFunction) {
+export default function authMiddleware(req: Request & {headers: Partial<RequestHeadersInterface>}, res: Response, next: NextFunction) {
   try {
     const token = req.get('token') || '';
     authService.verifyJWT(token);
