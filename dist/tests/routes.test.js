9b8f32f188e403919f793303fe18c898646c9d7a
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-26 21:31:56 +0200

    Se migra todo a JS

--- dist/tests/routes.test.js
+++ dist/tests/routes.test.js
@@ -90,9 +90,9 @@ describe("Bookings Testing", () => {
     }));
     it("should return deleted booking with DELETE method", () => __awaiter(void 0, void 0, void 0, function* () {
         const res = yield (0, supertest_1.default)(app_1.app)
-            .delete("/bookings/98765")
+            .delete("/bookings/24680")
             .set("token", authToken);
-        expect(res.body[0].guest.id_reserva).toEqual("98765");
+        expect(res.body[0].guest.id_reserva).toEqual("24680");
     }));
     it("should return updated booking with PUT method", () => __awaiter(void 0, void 0, void 0, function* () {
         const prev_res = yield (0, supertest_1.default)(app_1.app)
@@ -183,11 +183,11 @@ describe("Rooms Testing", () => {
             .set("token", authToken);
         expect(res.body[0].room_name.id).toEqual("1ABCD123");
     }));
-    it("should return deleted booking with DELETE method", () => __awaiter(void 0, void 0, void 0, function* () {
+    it("should return deleted room with DELETE method", () => __awaiter(void 0, void 0, void 0, function* () {
         const res = yield (0, supertest_1.default)(app_1.app)
-            .delete("/rooms/1ABCD123")
+            .delete("/rooms/2EFGH456")
             .set("token", authToken);
-        expect(res.body[0].room_name.id).toEqual("1ABCD123");
+        expect(res.body[0].room_name.id).toEqual("2EFGH456");
     }));
     it("should return updated room with PUT method", () => __awaiter(void 0, void 0, void 0, function* () {
         const prev_res = yield (0, supertest_1.default)(app_1.app)
@@ -243,7 +243,7 @@ describe("Contacts Testing", () => {
     }));
     it("should return all contacts with GET method", () => __awaiter(void 0, void 0, void 0, function* () {
         const res = yield (0, supertest_1.default)(app_1.app).get("/contacts").set("token", authToken);
-        expect(res.body[0]).toHaveProperty("offer_price");
+        expect(res.body[0]).toHaveProperty("comment");
     }));
     it("should add one contact with POST method", () => __awaiter(void 0, void 0, void 0, function* () {
         const res1 = yield (0, supertest_1.default)(app_1.app).get("/contacts").set("token", authToken);
@@ -275,9 +275,9 @@ describe("Contacts Testing", () => {
     }));
     it("should return deleted booking with DELETE method", () => __awaiter(void 0, void 0, void 0, function* () {
         const res = yield (0, supertest_1.default)(app_1.app)
-            .delete("/contacts/12345")
+            .delete("/contacts/67890")
             .set("token", authToken);
-        expect(res.body[0].date.id).toEqual("12345");
+        expect(res.body[0].date.id).toEqual("67890");
     }));
     it("should return updated room with PUT method", () => __awaiter(void 0, void 0, void 0, function* () {
         const prev_res = yield (0, supertest_1.default)(app_1.app)
@@ -304,5 +304,83 @@ describe("Contacts Testing", () => {
         const new_comment = res.body[0].comment;
         expect(prev_comment).not.toEqual(new_comment);
         expect(res.body[0].comment).toEqual("This is a modified contact");
+    }));
+});
+describe("Users Testing", () => {
+    let authToken;
+    beforeAll(() => __awaiter(void 0, void 0, void 0, function* () {
+        const res = yield (0, supertest_1.default)(app_1.app).post("/login").send({
+            user: "admin",
+            password: "admin",
+        });
+        authToken = res.body.token;
+    }));
+    it("should enter users", () => __awaiter(void 0, void 0, void 0, function* () {
+        const res = yield (0, supertest_1.default)(app_1.app).get("/users").set("token", authToken);
+        expect(res.statusCode).toEqual(200);
+    }));
+    it("should return all users with GET method", () => __awaiter(void 0, void 0, void 0, function* () {
+        const res = yield (0, supertest_1.default)(app_1.app).get("/users").set("token", authToken);
+        expect(res.body[0]).toHaveProperty("job_description");
+    }));
+    it("should add one contact with POST method", () => __awaiter(void 0, void 0, void 0, function* () {
+        const res1 = yield (0, supertest_1.default)(app_1.app).get("/users").set("token", authToken);
+        const initialLength = res1.body.length;
+        const res = yield (0, supertest_1.default)(app_1.app)
+            .post("/users")
+            .set("token", authToken)
+            .send({
+            name: {
+                photo: "https://robohash.org/employee1?set=set3.jpg",
+                username: "Angel Samuel",
+                id: "121dfeifnIF",
+                employee_position: "Manager",
+                email: "thisIsA@NewUser.com",
+                password_hash: "IDontLikeThis",
+            },
+            start_date: "2023-01-15",
+            job_description: "General hotel management, staff supervision, strategic decision-making.",
+            contact: "+1234567890",
+            activity: "active",
+        });
+        expect(res.body).toEqual(`Your user is number ${initialLength + 1}`);
+    }));
+    it("should return one contact with GET method", () => __awaiter(void 0, void 0, void 0, function* () {
+        const res = yield (0, supertest_1.default)(app_1.app)
+            .get("/users/121dfeifnIF")
+            .set("token", authToken);
+        expect(res.body[0].name.id).toEqual("121dfeifnIF");
+    }));
+    it("should return deleted booking with DELETE method", () => __awaiter(void 0, void 0, void 0, function* () {
+        const res = yield (0, supertest_1.default)(app_1.app)
+            .delete("/users/2abCdeFgH")
+            .set("token", authToken);
+        expect(res.body[0].name.id).toEqual("2abCdeFgH");
+    }));
+    it("should return updated room with PUT method", () => __awaiter(void 0, void 0, void 0, function* () {
+        const prev_res = yield (0, supertest_1.default)(app_1.app)
+            .get("/users/121dfeifnIF")
+            .set("token", authToken);
+        const prev_email = prev_res.body[0].name.email;
+        const res = yield (0, supertest_1.default)(app_1.app)
+            .put("/users/121dfeifnIF")
+            .set("token", authToken)
+            .send({
+            name: {
+                photo: "https://robohash.org/employee1?set=set3.jpg",
+                username: "Juan Rodriguez",
+                id: "121dfeifnIF",
+                employee_position: "Manager",
+                email: "thisIsA@UpdatedUser.com",
+                password_hash: "ILoveProgramming",
+            },
+            start_date: "2023-01-15",
+            job_description: "General hotel management, staff supervision, strategic decision-making.",
+            contact: "+1234567890",
+            activity: "active",
+        });
+        const new_email = res.body[0].name.email;
+        expect(prev_email).not.toEqual(new_email);
+        expect(res.body[0].name.email).toEqual("thisIsA@UpdatedUser.com");
     }));
 });
