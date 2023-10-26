7deb6130e0c4927e93612fc28567b98384276651
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-26 13:24:23 +0200

    Se crea ruta publica que muestra la info

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { loginController } from "./controllers/login";
 import authMiddleware from "./middleware/auth";
 import { contactsController } from "./controllers/contacts";
 import { usersController } from "./controllers/users";
+import infoJSON from "./data/Info.json"
 
 export const app = express()
 
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 // public routes & middleware
+app.use('/info', (req :Request, res: Response) => res.send(infoJSON))
 app.use('/login', loginController)
 app.use(authMiddleware)
 
