6d7150664ce72a270435d2c0ebc839585ba3842e
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-26 11:14:34 +0200

    Se termina controller de users

---
+++ src/controllers/users.ts
@@ -0,0 +1,55 @@
+import { Request, Response, Router } from "express";
+import { UserInterface } from "../models/Users";
+import { userService } from "../services/users";
+
+export const usersController = Router();   
+
+usersController.get("/", async (_req: Request, res: Response) => {
+  try {
+    const result = await userService.getAllUsers();
+    res.send(result);
+  } catch (error) {
+    res.status(500).send("Error al obtener los mensajes.");
+  }
+});
+
+usersController.get("/:id", async (req: Request, res: Response) => {
+    try {
+      const result = await userService.getOneUser(req.params.id);
+        res.send(result);
+
+    } catch (error) {
+      res.status(400).send(`${error}`);
+    }
+  }
+);
+
+usersController.delete("/:id", async (req: Request, res: Response) => {
+    try {
+      const result = await userService.delete(req.params.id);
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(400).send(`${error}`);
+    }
+  }
+);
+
+usersController.put("/:id", async (req: Request,res: Response) => {
+    try {
+      const result = await userService.updateUser(req.params.id, req.body);
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(400).send(`${error}`);
+    }
+  }
+);
+
+usersController.post("/", async (req: Request<UserInterface>, res: Response) => {
+    try {
+      const result = await userService.postNewUser(req.body);
+      res.status(200).send(`Your user is number ${result}`);
+    } catch (error) {
+      res.status(500).send(`${error}`);
+    }
+  }
+);
