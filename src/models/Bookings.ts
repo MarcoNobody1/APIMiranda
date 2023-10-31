6db16fd76538e91dd102ddebac9dcb3b04eec909
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-31 08:53:56 +0100

    Se modifica modelo de bookings para que se adapte a la logica de mongodb y se aplica typescript

--- src/models/Bookings.ts
+++ src/models/Bookings.ts
@@ -1,65 +1,27 @@
-import bookingsData from "../data/Bookings.json";
+import mongoose from "mongoose";
+const { Schema } = mongoose;
 import { BookingInterface } from "../interfaces/Bookings";
 
-export const bookings = bookingsData as BookingInterface[];
 
-async function getAllBookings() {
-  //logica futura en DB.
-  const data = await bookings;
-  if (data.length === 0) throw new Error("No existen reservas.");
-  return data;
-}
+const bookingsSchema = new Schema<BookingInterface>({
+  guest: {
+    nombre: String,
+    apellidos: String,
+    id_reserva: String,
+  },
+  order_date: String,
+  check_in: String,
+  check_out: String,
+  special_request: String,
+  room: {
+    id: String,
+    room_type: String,
+    room_number: String,
+    price: Number,
+    amenities: [String],
+    room_description: String,
+  },
+  status: String,
+});
 
-async function getOneBooking(bookingId: number) {
-  //logica futura en DB.
-  const data = await bookings.filter(
-    (booking) => booking.guest.id_reserva === bookingId.toString()
-  );
-  if (data.length === 0) throw new Error("No hay ninguna reserva con ese id.");
-  return data;
-}
-
-async function postNewBooking(booking: BookingInterface) {
-  //logica futura en DB.
-  const initialLength = bookings.length;
-  const data = await bookings.push(booking);
-  if(data === initialLength) throw new Error("Tu reserva no se añadio correctamente.")
-  return data;
-}
-
-async function updateBooking(
-  bookingId: number,
-  update: Partial<BookingInterface>
-) {
-  //logica futura en DB.
-  const bookingIndex = await bookings.findIndex(
-    (booking) => booking.guest.id_reserva === bookingId.toString()
-  );
-
-  if (bookingIndex === -1) throw new Error("No puedes modificar una reserva que no existe.")
-  const data = [...bookings];
-  Object.assign(data[bookingIndex], update);
-  return data;
-
-}
-
-async function deleteBooking(bookingId: number) {
-  //logica futura en DB.
-
-  const bookingIndex = await bookings.findIndex(
-    (booking) => booking.guest.id_reserva === bookingId.toString()
-  );
-
-  if (bookingIndex === -1) throw new Error("No hay ninguna reserva con ese id.");
-
-  const data = bookings.splice(bookingIndex, 1);
-  return data;
-}
-
-export const bookingService = {
-  getAllBookings,
-  getOneBooking,
-  postNewBooking,
-  updateBooking,
-  delete: deleteBooking,
-};
+export const Bookings = mongoose.model("Bookings", bookingsSchema);
