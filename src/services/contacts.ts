1af8a4cb81a43a2feb21dd118ca9db77a9004e78
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-31 12:40:44 +0100

    Se aplica modelo y se termina logica de service de Contacts

--- src/services/contacts.ts
+++ src/services/contacts.ts
@@ -1,60 +1,50 @@
-import contactsData from "../data/Contacts.json";
-import { ContactInterface } from "../models/Contacts.model";
+import { ContactInterface } from "../interfaces/Contacts";
+import { Contacts } from "../models/Contacts.model";
 
-export const contacts = contactsData as ContactInterface[];
 
 async function getAllContacts() {
-  //logica futura en DB.
-  const data = await contacts;
-  if (data.length === 0) throw new Error("No existen reservas.");
-  return data;
+  const contacts = await Contacts.find();
+  if (contacts.length === 0) throw new Error("Error al obtener los mensajes.");
+  return contacts;
 }
 
-async function getOneContact(contactId: number) {
-  //logica futura en DB.
-  const data = await contacts.filter(
-    (contact) => contact.date.id === contactId.toString()
-  );
-  if (data.length === 0) throw new Error("No hay ninguna reserva con ese id.");
-  return data;
+async function getOneContact(contactId: string) {
+  const contact = await Contacts.findById(contactId);
+  if (!contact) throw new Error("No hay ningun mensaje con ese id.");
+  return contact;
 }
 
 async function postNewContact(contact: ContactInterface) {
-  //logica futura en DB.
-  const initialLength = contacts.length;
-  const data = await contacts.push(contact);
-  if(data === initialLength) throw new Error("Tu reserva no se añadio correctamente.")
-  return data;
+  const newContact = await Contacts.create(contact);
+  if (!newContact) throw new Error("Tu mensaje no se añadio correctamente.");
+  return newContact;
 }
 
 async function updateContact(
-  contactId: number,
+  contactId: string,
   update: Partial<ContactInterface>
 ) {
-  //logica futura en DB.
-  const contactIndex = await contacts.findIndex(
-    (contact) => contact.date.id === contactId.toString()
-  );
+  const updatedContact = await Contacts.findByIdAndUpdate(contactId, update, {
+    new: true,
+  });
 
-  if (contactIndex === -1) throw new Error("No puedes modificar una reserva que no existe.")
-  const data = [...contacts];
-  Object.assign(data[contactIndex], update);
-  return data;
+  if (!updatedContact) {
+    throw new Error("No puedes modificar un mensaje que no existe.");
+  }
 
+  return updatedContact;
 }
 
-async function deleteContact(contactId: number) {
-  //logica futura en DB.
+async function deleteContact(contactId: string) {
+  const deletedContact = await Contacts.findByIdAndDelete(contactId);
 
-  const contactIndex = await contacts.findIndex(
-    (contact) => contact.date.id === contactId.toString()
-  );
+  if (!deletedContact) {
+    throw new Error("No hay ningun mensaje con ese id.");
+  }
 
-  if (contactIndex === -1) throw new Error("No hay ninguna reserva con ese id.");
-
-  const data = contacts.splice(contactIndex, 1);
-  return data;
+  return deletedContact;
 }
+
 
 export const contactService = {
   getAllContacts,
