f6d70ac7fc377c3bc819e1a5501252b85fdc8fa8
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-31 12:40:12 +0100

    Se termina modelo de Users

--- src/models/Users.model.ts
+++ src/models/Users.model.ts
@@ -1,65 +1,20 @@
-import usersData from "../data/Users.json";
+import mongoose from "mongoose";
+const { Schema } = mongoose;
 import { UserInterface } from "../interfaces/Users";
 
-export const users = usersData as UserInterface[];
+const usersSchema = new Schema<UserInterface>({
+  name: {
+    photo: { type: String, required: true },
+    username: { type: String, required: true },
+    id: { type: String, required: true },
+    employee_position: { type: String, required: true },
+    email: { type: String, required: true },
+    password_hash: { type: String, required: true },
+  },
+  start_date: { type: String, required: true },
+  job_description: { type: String, required: true },
+  contact: { type: String, required: true },
+  activity: { type: String, required: true },
+});
 
-async function getAllUsers() {
-  //logica futura en DB.
-  const data = await users;
-  if (data.length === 0) throw new Error("No existen reservas.");
-  return data;
-}
-
-async function getOneUser(userId: string) {
-  //logica futura en DB.
-  const data = await users.filter(
-    (user) => user.name.id === userId
-  );
-  if (data.length === 0) throw new Error("No hay ninguna reserva con ese id.");
-  return data;
-}
-
-async function postNewUser(user: UserInterface) {
-  //logica futura en DB.
-  const initialLength = users.length;
-  const data = await users.push(user);
-  if(data === initialLength) throw new Error("Tu reserva no se añadio correctamente.")
-  return data;
-}
-
-async function updateUser(
-  userId: string,
-  update: Partial<UserInterface>
-) {
-  //logica futura en DB.
-  const userIndex = await users.findIndex(
-    (user) => user.name.id === userId
-  );
-
-  if (userIndex === -1) throw new Error("No puedes modificar una reserva que no existe.")
-  const data = [...users];
-  Object.assign(data[userIndex], update);
-  return data;
-
-}
-
-async function deleteUser(userId: string) {
-  //logica futura en DB.
-
-  const userIndex = await users.findIndex(
-    (user) => user.name.id === userId
-  );
-
-  if (userIndex === -1) throw new Error("No hay ninguna reserva con ese id.");
-
-  const data = users.splice(userIndex, 1);
-  return data;
-}
-
-export const userService = {
-  getAllUsers,
-  getOneUser,
-  postNewUser,
-  updateUser,
-  delete: deleteUser,
-};
+export const Users = mongoose.model<UserInterface>("Users", usersSchema);
