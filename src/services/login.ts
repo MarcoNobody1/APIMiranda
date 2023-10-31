8db04bee9fa6b42b9cdf60d12a0d06a30c997877
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-31 10:58:19 +0100

    Se replantea la estructura del proyecto y se devuelve carpeta de services

---
+++ src/services/login.ts
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+const defaultUser = {
+  user: "admin",
+  password: "admin",
+};
+
+const secretToken: string = process.env.SECRET_TOKEN || "";
+
+async function login(user: string, password: string) {
+  if (defaultUser.user !== user || defaultUser.password !== password) {
+    throw new Error("Username or Password Incorrect!");
+  }
+  return await signJWT({ user });
+}
+
+function signJWT(payload: { user: string }) {
+  const token = jwt.sign(payload, secretToken, { expiresIn: "1h" });
+  return { payload, token };
+}
+
+function verifyJWT(token: string) {
+  const payload = jwt.verify(token, secretToken);
+  return payload;
+}
+
+const authService = {
+  login,
+  signJWT,
+  verifyJWT,
+};
+
+export default authService;
---
+++ src/services/users.ts
@@ -0,0 +1,65 @@
+import usersData from "../data/Users.json";
+import { UserInterface } from "../models/Users.model";
+
+export const users = usersData as UserInterface[];
+
+async function getAllUsers() {
+  //logica futura en DB.
+  const data = await users;
+  if (data.length === 0) throw new Error("No existen reservas.");
+  return data;
+}
+
+async function getOneUser(userId: string) {
+  //logica futura en DB.
+  const data = await users.filter(
+    (user) => user.name.id === userId
+  );
+  if (data.length === 0) throw new Error("No hay ninguna reserva con ese id.");
+  return data;
+}
+
+async function postNewUser(user: UserInterface) {
+  //logica futura en DB.
+  const initialLength = users.length;
+  const data = await users.push(user);
+  if(data === initialLength) throw new Error("Tu reserva no se añadio correctamente.")
+  return data;
+}
+
+async function updateUser(
+  userId: string,
+  update: Partial<UserInterface>
+) {
+  //logica futura en DB.
+  const userIndex = await users.findIndex(
+    (user) => user.name.id === userId
+  );
+
+  if (userIndex === -1) throw new Error("No puedes modificar una reserva que no existe.")
+  const data = [...users];
+  Object.assign(data[userIndex], update);
+  return data;
+
+}
+
+async function deleteUser(userId: string) {
+  //logica futura en DB.
+
+  const userIndex = await users.findIndex(
+    (user) => user.name.id === userId
+  );
+
+  if (userIndex === -1) throw new Error("No hay ninguna reserva con ese id.");
+
+  const data = users.splice(userIndex, 1);
+  return data;
+}
+
+export const userService = {
+  getAllUsers,
+  getOneUser,
+  postNewUser,
+  updateUser,
+  delete: deleteUser,
+};
