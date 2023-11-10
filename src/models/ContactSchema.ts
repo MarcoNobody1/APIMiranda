07c8070914f5397cf97ae67c9cb8660f248fd38d
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-11-10 09:30:10 +0100

    Se termina schema de contact

--- src/models/ContactSchema.ts
+++ src/models/ContactSchema.ts
@@ -1,4 +1,11 @@
 import Joi from "joi";
 
 export const ContactSchema = Joi.object({
+  date: Joi.date().required(),
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  subject: Joi.string().required(),
+  comment: Joi.string().required(),
+  archived: Joi.boolean().required(),
 });
