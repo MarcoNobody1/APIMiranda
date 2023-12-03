4249ab4777e98c5d71e747dcd2ab3e62cccbb7cf
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-03 05:15:34 +0100

    se corrige controlador

--- src/controllers/email.ts
+++ src/controllers/email.ts
@@ -3,10 +3,10 @@ import { sendEmailService } from "../services/email";
 
 export const emailController = Router();
 
-emailController.post("/send", async (req: Request, res: Response) => {
+emailController.post("/", async (req: Request, res: Response) => {
   try {
-    const { to, subject, text } = req.body;
-    await sendEmailService.sendEmail(to, subject, text);
+    const { subject, text } = req.body;
+    await sendEmailService.sendEmail(subject, text);
     res.json({ message: "Email sent successfully" });
   } catch (error) {
     console.error(error);
