15473268e7403a360cfedb326e23e3ef94cfea80
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-03 05:19:35 +0100

    se corrige service

--- src/services/email.ts
+++ src/services/email.ts
@@ -5,7 +5,7 @@ const sender = process.env.EMAIL_SENDER || "";
 const password = process.env.EMAIL_PASSWORD || "";
 const receiver = process.env.EMAIL_RECEIVER || "";
 
-async function sendEmail(to: string, subject: string, text: string) {
+async function sendEmail( subject: string, text: string) {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
