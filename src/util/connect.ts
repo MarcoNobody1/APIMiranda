bfed19d9b38101aab3a9148cb607627d6ae11367
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-20 15:07:43 +0100

    changes varios

--- src/util/connect.ts
+++ src/util/connect.ts
@@ -1,22 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import "dotenv/config";
-import { ServerApiVersion } from 'mongodb';
+import { ServerApiVersion } from "mongodb";
 
-const serverHost: string = process.env.ATLAS_SERVER || '';
+const serverHost: string = process.env.ATLAS_SERVER || "";
 const databaseName: string = process.env.DB_NAME || "";
 export const ConnectToDatabase = async () => {
-    console.log(serverHost)
-    try {
-      await mongoose.connect(serverHost, {
-        dbName: databaseName,
-        serverApi: {
-          version: ServerApiVersion.v1,
-          strict: true,
-          deprecationErrors: true,
-        },
-      });
-      console.log("CONNECTED");
-    } catch (error) {
-      throw new Error(`${error}`);
-    }
+  console.log(serverHost);
+  try {
+    await mongoose.connect(serverHost, {
+      dbName: databaseName || "MirandaDashboard",
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
+    console.log("CONNECTED");
+  } catch (error) {
+    throw new Error(`${error}`);
   }
+};
