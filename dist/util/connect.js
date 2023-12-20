438034e60f137b4330f5a8eaaa7dae0522cd7d36
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-20 15:37:37 +0100

    dist configure & name deploy

--- dist/util/connect.js
+++ dist/util/connect.js
@@ -7,13 +7,13 @@ exports.ConnectToDatabase = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 require("dotenv/config");
 const mongodb_1 = require("mongodb");
-const serverHost = process.env.ATLAS_SERVER || '';
+const serverHost = process.env.ATLAS_SERVER || "";
 const databaseName = process.env.DB_NAME || "";
 const ConnectToDatabase = async () => {
     console.log(serverHost);
     try {
         await mongoose_1.default.connect(serverHost, {
-            dbName: databaseName,
+            dbName: databaseName || "MirandaDashboard",
             serverApi: {
                 version: mongodb_1.ServerApiVersion.v1,
                 strict: true,
